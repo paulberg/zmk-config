b84f72a2839e715ca242b33a11ad9e9f1903e19a
Paul Berg <paul@paulwberg.com>
2024-02-24 03:57:06 -0800

    Adding combos

--- config/keymap_base.h
+++ config/keymap_base.h
@@ -17,6 +17,7 @@
 #include <behaviors.dtsi>
 #include <dt-bindings/zmk/keys.h>
 #include "extra_keys.h"
+#include "timing.h"
 
 #ifdef CONFIG_WIRELESS
     #include <dt-bindings/zmk/bt.h>
@@ -32,6 +33,67 @@
 #define NUM 2
 #define NAV 3
 #define SYS 4
+
+// Includes needing layers defined
+#include "combos.h"
+
+//////////////////////////////
+// Global behavior defalts
+
+// mods deactivate caps-word, requires PR #1451
+&caps_word {  
+  /delete-property/ ignore-modifiers;
+};
+
+// mod-tap confg
+&mt { 
+    flavor = "tap-preferred";
+    tapping-term-ms = <TAPPING_TERM>;
+    quick-tap-ms = <TAPPING_TERM>;
+    hold-trigger-key-positions = <0>;
+};
+
+// layer-tap config
+&lt {
+    flavor = "balanced";
+    tapping-term-ms = <TAPPING_TERM>;
+    quick-tap-ms = <QUICK_TAP_TERM>;
+};
+
+// sticky-key config
+&sk {
+    release-after-ms = <STICKY_TERM>;
+
+    // no double capitalization when rolling keys
+    quick-release;             
+};
+
+// sticky-layer config
+&sl {
+    // allow chording sticky mods & layers
+    ignore-modifiers;
+};
+
+// Macro to create behaviors for home row mods by reducing cross hand false negatives
+#define MAKE_HRM(NAME, HOLD, TAP, TRIGGER_POS) \
+  ZMK_BEHAVIOR(NAME, hold_tap, \
+    flavor = "balanced"; \
+    tapping-term-ms = <TAPPING_TERM>; \
+    quick-tap-ms = <QUICK_TAP_TERM>; \
+    require-prior-idle-ms = <REQUIRE_PRIOR_IDLE_TERM>; \
+    bindings = <HOLD>, <TAP>; \
+    hold-trigger-key-positions = <TRIGGER_POS>; \
+    hold-trigger-on-release; \
+  )
+
+// Left and right hand home row mods
+MAKE_HRM(hml, &kp, &kp, KEYS_R THUMBS)
+MAKE_HRM(hmr, &kp, &kp, KEYS_L THUMBS)
+
+// Used for HRM combos
+MAKE_HRM(hml_repeat, &kp, &key_repeat, KEYS_R THUMBS)
+MAKE_HRM(hmr_lpar_lt, &kp, &lpar_lt, KEYS_L THUMBS)
+MAKE_HRM(hmr_rpar_gt, &kp, &rpar_gt, KEYS_L THUMBS)
 
 // tap: comma | shift + tap: semicolon
 SIMPLE_MORPH(comma_morph, SFT, &kp COMMA, &kp SEMICOLON)
---
+++ config/timing.h
@@ -0,0 +1,41 @@
+// Timing config for keystrokes
+// Copyright Paul W. Berg
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/////////////////
+// All times are in ms
+
+// When typing a combo, all keys must be pressed within the term.
+// Fast and slow varients
+#define COMBO_TERM_FAST 18
+#define COMBO_TERM_SLOW 30
+
+// If you press a tapped hold-tap again within the term of the first press,
+// it will always trigger the tap behavior. This is useful for things like a
+// backspace, where a quick tap+hold holds backspace pressed.
+// Set this to a negative value to disable.
+#define QUICK_TAP_TERM 175
+
+// Similar to QUICK_TAP_TERM, however it will apply for any non-modifier key
+// pressed before it. This effectively disables the hold-tap when typing quickly,
+// which can be quite useful for homerow mods. It can also have the effect of
+// removing the input delay when typing quickly.
+#define REQUIRE_PRIOR_IDLE_TERM 150
+
+// How long a sticky key stays active before releasing if no other key pressed.
+#define STICKY_TERM 1000
+
+// Defines how long a key must be pressed to trigger Hold behavior.
+#define TAPPING_TERM 220
